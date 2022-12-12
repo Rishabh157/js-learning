364cd2010c2177f302e01e437ef71dec492fc5d9
Rishabh157 <rishabhgour157@gmail.com>
2022-12-12 11:29:38 +0530

    itration and problem file added

---
+++ itration.js
@@ -0,0 +1,25 @@
+//         0  1  2  3  4  5  6  7  8  9
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// for 
+// 3
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`${i} =>  ${arr[i]}  `)
+// }
+
+
+// while
+
+let i = 0
+
+while (i < arr.length) {
+
+    console.log(arr[i])
+
+    i++
+}
+
+// js looping (itration)  ->  map(), forEach() , reduce() , filter()
+
---
+++ problem.js
@@ -0,0 +1,49 @@
+// 1.) 
+
+// let num1 = 10
+// let num2 = 20
+
+// let temp = num1
+// num1 =  num2
+
+// num2 = temp
+
+// console.log(num1)
+// console.log(num2)
+
+// ------------------------------
+
+
+
+
+// let num = 3
+
+// let ans = num%2===0 ? "even" : "odd"
+
+// console.log(ans)
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let sum = arr.reduce((accum, ele) => {
+//     return accum += ele
+// }, 0)
+
+// console.log(sum)
+
+// let num = 2
+
+// let ans = num % 10 === 0 ? "ok" : "not divisiable"
+
+// console.log(ans)
+
+
+// It is because the freezing point of water in Fahrenheit is 32F and in Celsius is 0C.
+// Also, the boiling point of water in Fahrenheit is 212F and in Celsius is 100C.
+// Based on the above facts and figures, the conversion formulae from one to the other either add or subtract 32.
+
+
+// let celcius = 60
+// let feranheight = celcius * 1.8 + 32
+// console.log(feranheight)
